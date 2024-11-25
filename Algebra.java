f139253143468f4a8829542c5a9ab0c74f1f24d1
nocode-1 <nathan.orlowski@post.runi.ac.il>
2024-11-25 17:04:29 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -3,65 +3,83 @@
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
 
-public class Algebra {
-	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+public class first {
+    public static void main(String args[]) {
+        // Tests some of the operations
 
-	// Returns x1 + x2
-	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+        System.out.println(plus(2, 3)); // 2 + 3
+        System.out.println(minus(7, 2)); // 7 - 2
+        System.out.println(minus(2, 7)); // 2 - 7
+        System.out.println(times(3, 4)); // 3 * 4
+        System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
+        System.out.println(pow(5, 3)); // 5^3
+        System.out.println(pow(3, 5)); // 3^5
+        System.out.println(div(12, 3)); // 12 / 3
+        System.out.println(div(5, 5)); // 5 / 5
+        System.out.println(div(25, 7)); // 25 / 7
+        System.out.println(mod(25, 7)); // 25 % 7
+        System.out.println(mod(120, 6)); // 120 % 6
+        System.out.println(sqrt(36));
+        System.out.println(sqrt(263169));
+        System.out.println(sqrt(76123));
+    }
 
-	// Returns x1 - x2
-	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x1 + x2
+    public static int plus(int x1, int x2) {
+        int sum = x1;
+        for (int i = 0; i < x2; i++) {
+            sum++;
+        }
+        // Replace the following statement with your code
+        return sum;
+    }
 
-	// Returns x1 * x2
-	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x1 - x2
+    public static int minus(int x1, int x2) {
+        int sum = x1;
+        for (int i = 0; i < x2; i++) {
+            sum--;
+        }
+        // Replace the following statement with your code
+        return sum;
+    }
 
-	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x1 * x2
+    public static int times(int x1, int x2) {
+        int product = 0;
+        for (int i = 0; i < x2; i++) {
+            product += x1;
+        }
+        return product;
+    }
 
-	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}
+    // Returns x^n (for n >= 0)
+    // 5^3
+    public static int pow(int x, int n) {
+        int product = 1;
+        if (n >= 0) {
+            for (int i = 0; i < n; i++) {
+                product *= x;
+            }
+        }
+        return product;
+    }
 
-	// Returns x1 % x2
-	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+    // Returns the integer part of x1 / x2
+    public static int div(int x1, int x2) {
+      // Replace the following statement with your code
+      return 0;
+    }
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+    // Returns x1 % x2
+    public static int mod(int x1, int x2) {
+        // Replace the following statement with your code
+        return 0;
+    }
+
+    // Returns the integer part of sqrt(x)
+    public static int sqrt(int x) {
+        // Replace the following statement with your code
+        return 0;
+    }
 }
