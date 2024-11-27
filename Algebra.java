ff15b7792d57c512fbf1008691f9038cd7d30d7d
nocode-1 <nathan.orlowski@post.runi.ac.il>
2024-11-27 12:28:30 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -3,7 +3,7 @@
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
 
-public class Algebra {
+public class first {
     public static void main(String args[]) {
         // Tests some of the operations
 
@@ -30,6 +30,10 @@ public class Algebra {
         for (int i = 0; i < x2; i++) {
             sum++;
         }
+
+        if (sum < 0) {
+            return sum;
+        }
         // Replace the following statement with your code
         return sum;
     }
@@ -37,9 +41,18 @@ public class Algebra {
     // Returns x1 - x2
     public static int minus(int x1, int x2) {
         int sum = x1;
+        
         for (int i = 0; i < x2; i++) {
             sum--;
         }
+        if (x1 < 0 && x2 < 0) {
+            for (int i = 0; i < x2; i++) {
+                sum--;
+            } 
+        }
+
+        
+
         // Replace the following statement with your code
         return sum;
     }
@@ -61,6 +74,8 @@ public class Algebra {
             for (int i = 0; i < n; i++) {
                 product *= x;
             }
+        } else {
+            return 0;
         }
         return product;
     }
@@ -68,8 +83,13 @@ public class Algebra {
     // Returns the integer part of x1 / x2
     public static int div(int x1, int x2) {
         if (x2 == 0) {
-            return 0;
+            return error;
         }
+
+        boolean isNegative = (x1 < 0) ^ (x2 < 0);
+
+        x1 = Math.abs(x1);
+        x2 = Math.abs(x2);
 
         int quotient = 0;
 
@@ -81,7 +101,7 @@ public class Algebra {
 
     }
 
-    // Returns x1 % x2
+    // Returns x1 %  x2
     public static int mod(int x1, int x2) {
         // Replace the following statement with your code
         return 0;
