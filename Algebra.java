2edb2890a37c39e16c0a2a5349ab42986a8e0611
nocode-1 <nathan.orlowski@post.runi.ac.il>
2024-11-27 12:50:20 +0200

    Update Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -27,44 +27,53 @@ public class Algebra {
     // Returns x1 + x2
     public static int plus(int x1, int x2) {
         int sum = x1;
-        for (int i = 0; i < x2; i++) {
-            sum++;
+    
+        if (x2 < 0) {
+            for (int i = 0; i < -x2; i++) {
+                sum--;
+            }
+        } else { 
+            for (int i = 0; i < x2; i++) {
+                sum++;
+            }
         }
-
-        if (sum < 0) {
-            return sum;
-        }
-        // Replace the following statement with your code
+    
         return sum;
     }
 
-    // Returns x1 - x2
     public static int minus(int x1, int x2) {
         int sum = x1;
-        
-        for (int i = 0; i < x2; i++) {
-            sum--;
-        }
-        if (x1 < 0 && x2 < 0) {
+    
+        if (x2 < 0) {
+            for (int i = 0; i < -x2; i++) {
+                sum++;
+            }
+        } else {
             for (int i = 0; i < x2; i++) {
                 sum--;
-            } 
+            }
         }
-
-        
-
-        // Replace the following statement with your code
+    
         return sum;
     }
 
-    // Returns x1 * x2
+        
     public static int times(int x1, int x2) {
         int product = 0;
-        for (int i = 0; i < x2; i++) {
-            product += x1;
+    
+        if (x2 < 0) {
+            for (int i = 0; i < -x2; i++) {
+                product -= x1;
+            }
+        } else {
+            for (int i = 0; i < x2; i++) {
+                product += x1;
+            }
         }
+    
         return product;
     }
+
 
     // Returns x^n (for n >= 0)
     // 5^3
@@ -97,13 +106,32 @@ public class Algebra {
     }
     // Returns x1 %  x2
     public static int mod(int x1, int x2) {
-        // Replace the following statement with your code
-        return 0;
+        int remainder = x1;
+    
+        while (remainder >= x2) {
+            remainder -= x2;
+        }
+    
+        return remainder;
     }
 
     // Returns the integer part of sqrt(x)
     public static int sqrt(int x) {
-        // Replace the following statement with your code
-        return 0;
+        if (x == 0 || x == 1) {
+            return x;
+        }
+    
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid < x) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right;
     }
 }
