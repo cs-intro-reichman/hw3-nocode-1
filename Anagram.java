f0bf1b758f4165217f326dc246be5bfacf0d6d67
nocode-1 <nathan.orlowski@post.runi.ac.il>
2024-11-28 11:09:52 +0200

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -3,7 +3,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Anagram {
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         // Tests the isAnagram function.
         System.out.println(isAnagram("silent", "listen")); // true
         System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
@@ -18,7 +18,7 @@ public class Anagram {
 
         // Performs a stress test of randomAnagram
         String str = "1234567";
-        boolean pass = true;
+        Boolean pass = true;
         //// 10 can be changed to much larger values, like 1000
         for (int i = 0; i < 10; i++) {
             String randomAnagram = randomAnagram(str);
@@ -29,32 +29,26 @@ public class Anagram {
         }
         System.out.println(pass ? "test passed" : "test Failed");
     }
-
     public static boolean isAnagram(String str1, String str2) {
-        // Preprocess both strings to remove non-alphabetic characters and convert to
-        // lowercase
+        // Preprocess both strings
         String processedStr1 = preProcess(str1);
         String processedStr2 = preProcess(str2);
 
-        // checking length here
+        // Check lengths
         if (processedStr1.length() != processedStr2.length()) {
             return false;
         }
 
-        // amount of char to se if it is an Anagram in the first place
+        //  charakter count  for comparison
         int[] charCount = new int[26];
         for (char c : processedStr1.toCharArray()) {
-            if (c != ' ') { // Skip spaces
-                charCount[c - 'a']++;
-            }
+            charCount[c - 'a']++;
         }
 
         for (char c : processedStr2.toCharArray()) {
-            if (c != ' ') { // skip space
-                charCount[c - 'a']--;
-                if (charCount[c - 'a'] < 0) {
-                    return false;
-                }
+            charCount[c - 'a']--;
+            if (charCount[c - 'a'] < 0) {
+                return false;
             }
         }
 
@@ -66,8 +60,6 @@ public class Anagram {
         for (char c : str.toCharArray()) {
             if (Character.isLetter(c)) {
                 sb.append(Character.toLowerCase(c));
-            } else if (c == ' ') {
-                sb.append(c); 
             }
         }
         return sb.toString();
